070ce004de7b81be316f088bfc59652b4c30e922
giovannitecads <giovanni.slopess@gmail.com>
2026-09-09 22:39:08 -0300

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom/cliente";
+import App from "App";
+import "bootstrap/dist/css/bootstrap.min.cs";
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+        <App/>
+    </React.StrictMode>
+);
+
